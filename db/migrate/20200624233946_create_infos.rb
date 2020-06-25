10f0c7f6f2aa296be5ceed8c1a1aa58d3ab5e5b9
EC2 Default User <ec2-user@ip-172-31-40-194.ap-northeast-1.compute.internal>
2020-06-25 00:01:38 +0000

    infoテーブルにカラムを追加

--- db/migrate/20200624233946_create_infos.rb
+++ db/migrate/20200624233946_create_infos.rb
@@ -1,7 +1,10 @@
 class CreateInfos < ActiveRecord::Migration[6.0]
   def change
     create_table :infos do |t|
-
+      t.integer    :user_id
+      t.integer    :topic_id
+      t.text       :title
+      t.text       :text
       t.timestamps
     end
   end
