c68b5ac6cbd8ac1d6ae20c6305bb3a2d37e6b850
EC2 Default User <ec2-user@ip-172-31-40-194.ap-northeast-1.compute.internal>
2020-06-25 00:08:09 +0000

    topicテーブルにカラムを追加

--- db/migrate/20200624233633_create_topics.rb
+++ db/migrate/20200624233633_create_topics.rb
@@ -1,7 +1,8 @@
 class CreateTopics < ActiveRecord::Migration[6.0]
   def change
     create_table :topics do |t|
-
+      t.text    :title
+      t.text    :text 
       t.timestamps
     end
   end
